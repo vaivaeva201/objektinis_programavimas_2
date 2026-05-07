8f8c7d4230f43f98715a6a7d9c295ea8c19ca2cc
Vaiva Evaltaitė <vaiva.evaltaite@gmail.com>
2026-05-07 17:33:46 +0300

    padaryti unit testai

--- catch_testai.cpp
+++ catch_testai.cpp
@@ -7,7 +7,7 @@ TEST_CASE("Studento klasės 'Rule of five' testai", "[studentas]")
     SECTION("Copy konstruktorius")
     {
         vector<int> pazymiai = {9, 8, 7, 6, 9};
-        Studentas originalus("Vardas", "Pavardė", pazymiai);
+        Studentas originalus("Vardas", "Pavarde", pazymiai);
         Studentas kopija(originalus);
         
         REQUIRE(kopija == originalus);
@@ -16,11 +16,42 @@ TEST_CASE("Studento klasės 'Rule of five' testai", "[studentas]")
     SECTION("Move konstruktorius")
     {
         vector<int> pazymiai = {9, 8, 7, 6, 9};
-        Studentas  originalus("Vardas", "Pavardė", pazymiai);
-        Studentas  originalus_2("Vardas", "Pavardė", pazymiai);
+        Studentas  originalus("Vardas", "Pavarde", pazymiai);
+        Studentas  originalus_2("Vardas", "Pavarde", pazymiai);
         Studentas perkeltas(std::move(originalus));
 
         REQUIRE(perkeltas == originalus_2);
+        REQUIRE(originalus.Clear() == true);
+    }
+
+    SECTION("Copy assignment operatorius")
+    {
+        vector<int> pazymiai = {9, 8, 7, 6, 9};
+        Studentas originalus("Vardas", "Pavarde", pazymiai);
+        Studentas kopija;
+        kopija = originalus;
+        
+        REQUIRE(kopija == originalus);
+    }
+
+    SECTION("Move assignment operatorius")
+    {
+        vector<int> pazymiai = {9, 8, 7, 6, 9};
+        Studentas  originalus("Vardas", "Pavarde", pazymiai);
+        Studentas  originalus_2("Vardas", "Pavarde", pazymiai);
+        Studentas perkeltas;
+        perkeltas= std::move(originalus);
+
+        REQUIRE(perkeltas == originalus_2);
+        REQUIRE(originalus.Clear() == true);
+    }
+
+    SECTION("Destruktorius")
+    {
+        Studentas  originalus("Vardas", "Pavarde", {10, 9});
+
+        originalus.Clear();
+
         REQUIRE(originalus.Clear() == true);
     }
 }
